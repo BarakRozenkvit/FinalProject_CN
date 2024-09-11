7fc3c20a12c59cf2e98795e2e4a40f1c54d5009c
BarakRozenkvit <barak10101998@gmail.com>
2024-09-11 21:07:13 +0300

    producer consumer

---
+++ FileBuffer.py
@@ -0,0 +1,147 @@
+import random
+import sys
+import time
+from threading import Thread, Lock
+from QUIC import Stream
+from FileHandler import FileHandler
+import queue
+
+
+class FileBuffer:
+
+    def __init__(self,path):
+        """
+        :param path: path to file
+        self.buffer: [(data1,seqNum1),(data2,seqNum2),(data3,seqNum3)]
+        self.streamID: uniqe ID to assign Stream Object
+        self.packageSize: size of each package of data from file
+        self.fileHandler: FileHandler Object
+        """
+        self.buffer = queue.Queue(maxsize=10)
+        self.packageSize = random.randint(1000,2000)
+        self.streamID = random.randint(1,2000)
+        self.fileHandler = FileHandler(path)
+
+    def isEmpty(self):
+        """
+        this function checks if buffer is empty
+        :return:
+        """
+        return self.buffer.empty()
+
+    def fillBuffer(self):
+        """
+        this function fills the buffer
+        :return:
+        """
+        if not (self.fileHandler.EOF):
+            data = (self.fileHandler.getData(self.packageSize),self.fileHandler.getSequenceNumber())
+            self.buffer.put(data)
+
+    def toStream(self):
+        """
+        1. pop 0 index from list (Queue)
+        2. convert to Stream
+        :return: Stream Object
+        """
+        data = self.buffer.get()
+        return Stream(self.streamID,data[1],len(data[0]),data[0])
+
+    @staticmethod
+    def toFileBuffer(stream):
+        """
+        convert the stream to FileBuffer
+        :param stream: Stream Object
+        :return: FileBuffer Object
+        """
+        # open(stream.stream_id + ".txt","w").close()
+        fileBuffer = FileBuffer("")
+        fileBuffer.streamID = stream.stream_id
+        fileBuffer.packageSize = stream.length
+        fileBuffer.buffer = [stream.stream_data]
+        return fileBuffer
+
+class BufferManager:
+
+    def __init__(self,file_list):
+        """
+        1. initialize the file list to empty
+        2. for every file in file list create FileBuffer object
+        :param file_list: list of files
+        """
+        self.fileBuffers = []
+        self.minPackageSize = 2000
+        self.res = 0
+        for i in range(0, len(file_list)):
+            fileBuffer = FileBuffer(file_list[i])
+            self.res += fileBuffer.fileHandler.fileSize
+            if fileBuffer.packageSize < self.minPackageSize:
+                self.minPackageSize = fileBuffer.packageSize
+            self.fileBuffers.append(fileBuffer)
+        self.running = True
+
+    def manage(self):
+        """
+        run this process while the flag self.running is True
+        for each buffer, if its empty or below threshold,
+        lock the buffer to the thread, fill it and release the buffer
+        :return: void
+        """
+        while self.running:
+            for fileBuffer in self.fileBuffers:
+                fileBuffer.fillBuffer()
+
+    def pack(self,payload_size):
+        """
+        while the payload size is greater than the minimum of package size of all the packages of all files
+        and self.running is True:
+        1. shuffle the list, for every buffer, lock the buffer to thread
+        2. if fileBuffer is not empty and the package size is smaller than payload size, convert to Stream and
+        add to list
+        3. if all buffers are empty stop this function
+        :return: Stream array
+        """
+        streamsToSend = []
+        while payload_size > self.minPackageSize and self.running:
+            EmptyBuffers = True
+            random.shuffle(self.fileBuffers)
+            for fileBuffer in self.fileBuffers:
+                if (not fileBuffer.isEmpty()) and fileBuffer.packageSize < payload_size:
+                    EmptyBuffers = False
+                    streamsToSend.append(fileBuffer.toStream())
+                    payload_size -= fileBuffer.packageSize
+            if EmptyBuffers:
+                break
+        return streamsToSend
+
+
+
+
+if __name__ == '__main__':
+    b = BufferManager(["Files/1.txt","Files/2.txt","Files/3.txt","Files/4.txt","Files/5.txt"])
+    t = Thread(target=b.manage)
+    t.start()
+    i=0
+    g=[1]
+    while g!=[]:
+        time.sleep(0.00000005)
+        g = b.pack(5000)
+        i+=1
+        t = 0
+        for l in g:
+            t+=l.length
+
+        print(t,g)
+
+    print(i)
+    for f in b.fileBuffers:
+        print(f.fileHandler.EOF)
+
+    b.running = False
+
+
+
+
+
+
+
